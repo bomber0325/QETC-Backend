fa85bc2727d111c010d4ca5d9b9e69b0d054f038
bomber0325 <codeguy00@gmail.com>
2023-02-27 15:04:16 +0200

    spcifying 'table names' in all models except for user/users

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "User",
-      tableName: "uusers",
+      // tableName: "uusers",
     }
   );
   return User;
