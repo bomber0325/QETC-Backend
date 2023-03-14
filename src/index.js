59d98eb074364419edf6ee72712adf699a1a995a
bomber0325 <codeguy00@gmail.com>
2023-03-14 03:08:46 -0400

    feat:update backend

--- src/index.js
+++ src/index.js
@@ -14,17 +14,6 @@ models.sequelize.sync().then(function () {
 
 });
 
-<<<<<<< HEAD
-
-=======
-app.on('request', (req, res) => {
-  if (req.url === '/') {
-      res.writeHead(200, {'content-type': 'text/html'})
-      res.write('<h2>Home Page</h2>')
-      res.end()
-  }
-})
->>>>>>> main
 
 console.log(`servers is running on ${port}` )
 
