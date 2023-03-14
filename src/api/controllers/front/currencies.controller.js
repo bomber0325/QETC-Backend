b588d52f1d536af851be9afacb8d519a115cf12b
bomber0325 <codeguy00@gmail.com>
2023-03-14 14:25:54 -0400

    feat: update backend

--- src/api/controllers/front/currencies.controller.js
+++ src/api/controllers/front/currencies.controller.js
@@ -133,6 +133,87 @@ exports.edit = async (req, res, next) => {
   }
 };
 
+exports.lists = async (req, res, next) => {
+  try {
+    const uni = await Currency.findAndCountAll();
+
+    let { page, limit, name } = req.query;
+
+    console.log("unitt", uni.count);
+    console.log("req.queryy", req.query); //name
+    const filter = {};
+
+    page = page !== undefined && page !== "" ? parseInt(page) : 1;
+    limit = limit !== undefined && limit !== "" ? parseInt(limit) : 10;
+
+    if (name) {
+      filter.name = { $LIKE: name, $options: "gi" };
+    }
+
+    const total = uni.count;
+
+    if (page > Math.ceil(total / limit) && total > 0)
+      page = Math.ceil(total / limit);
+
+    console.log("filter", filter);
+    const faqs = await Currency.findAll({
+      order: [["updatedAt", "DESC"]],
+      offset: limit * (page - 1),
+      limit: limit,
+      where: filter,
+    });
+    console.log("faqs", faqs);
+    // res.send(uni);
+    return res.send({
+      success: true,
+      message: "currency fetched successfully",
+      data: {
+        faqs,
+        pagination: {
+          page,
+          limit,
+          total,
+          pages: Math.ceil(total / limit) <= 0 ? 1 : Math.ceil(total / limit),
+        },
+      },
+    });
+  } catch (err) {
+    res.send("currency Error " + err);
+  }
+  // next();
+};
+
+// API to edit currency
+exports.edit = async (req, res, next) => {
+  try {
+    let payload = req.body;
+    console.log("edit payload =>", payload);
+
+    const currency = await Currency.update(
+      // Values to update
+      payload,
+      {
+        // Clause
+        where: {
+          id: payload.id,
+        },
+      }
+    );
+
+    console.log("edit cur currrr =>", currency);
+    await Activity.create({ action: "New Currency updated", name: req.body.Uname, role: req.body.role });
+
+    return res.send({
+      success: true,
+      message: "currency updated successfully",
+      currency,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+
 // API to delete currency
 exports.delete = async (req, res, next) => {
   try {
--- src/api/routes/v1/front/currencies.route.js
+++ src/api/routes/v1/front/currencies.route.js
@@ -5,6 +5,7 @@ const { cpUpload } = require("../../../utils/upload");
 
 router.route("/create").post(cpUpload, controller.create);
 router.route("/list").get(controller.list);
+router.route("/lists").get(controller.lists);
 router.route("/edit").put(cpUpload, controller.edit);
 router.route("/delete/:id").delete(controller.delete);
 router.route("/get").get(controller.get);
