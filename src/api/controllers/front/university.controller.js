33e6a1ac540764e7a3e1d7794f209e698982fd2b
bomber0325 <codeguy00@gmail.com>
2023-03-15 01:56:06 -0400

    feat: change backend sta

--- src/api/controllers/front/university.controller.js
+++ src/api/controllers/front/university.controller.js
@@ -32,7 +32,6 @@ exports.create = async (req, res, next) => {
     const newArr = JSON.parse(req.body.campuses);
     // await newArr.foreach(async (ele) => {
     for (i = 0; i < newArr.length; i++) {
-      console.log("array ********************* /n", ele);
       let campus = {
         name: newArr[i].name,
         address1: newArr[i].address1,
